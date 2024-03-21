bfe3b6066281fed2d8a549e33aebe3524b105e17
Blaze0341 <99755932+Blaze0341@users.noreply.github.com>
2024-03-21 14:13:10 -0400

    Update fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Blaze'
+description 'Fire script for FiveM'
+version '1.0.0'
+
+-- Specify the resource manifest version
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Specify the files that should be included in the resource
+files {
+    'config.lua',
+    'client.lua',
+    'server.lua'
+}
+
+-- Specify the client script
+client_script 'client.lua'
+
+-- Specify the server script
+server_script 'server.lua'
+
+-- Specify any dependencies for the resource
+dependency 'qb-core' -- Example dependency (replace with actual dependencies)
